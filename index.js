9fdaacb8ccab7afbedfc0d41643cdf87dce8ce33
Alessia <alessia.ruberto04@gmail.com>
2024-07-08 14:52:54 -0400

    Added letter animations and header gradient. Also fixed issue with logo collapsing.

--- index.js
+++ index.js
@@ -7,17 +7,24 @@ function checkViewportWidth() {
     const landing_left = document.querySelector('.landing-left');
     const landing = document.querySelector('.landing');
     const headshot = document.querySelector('.landing .headshot');
+    const landing_h1 = document.querySelector('.landing h1');
+    const header = document.querySelector('header');
+
     if (landing_left) {
         if (window.innerWidth < MOBILE_WIDTH) {
             landing_left.style.width = '90%';
             landing.style.flexWrap = 'wrap';
             headshot.style.marginTop = '15%';
             headshot.style.maxWidth = '40vw';
+            landing_h1.style.fontSize = '15vw';
+            header.style.marginBottom = '-40%'
         } else {
             landing_left.style.width = '60%';
             landing.style.flexWrap = 'nowrap';
             headshot.style.marginTop = '0';
             headshot.style.maxWidth = '350px';
+            landing_h1.style.fontSize = '7vw';
+            header.style.marginBottom = '-15%'
         }
     }
 }
@@ -37,4 +44,13 @@ function debounce(func, wait) {
     };
 }
 
+document.addEventListener('DOMContentLoaded', function() {
+    const images = document.querySelectorAll('.glass-letters img');
+    images.forEach(image => {
+        const randomDelay = Math.random(); // Random delay between 0 and 5 seconds
+        image.style.animationDelay = `${randomDelay}s`;
+    });
+});
+
 window.addEventListener('resize', debounce(checkViewportWidth, 100));
+window.addEventListener('load', checkViewportWidth);
